ed68b29346f08fb49cc0d9425ed7f4af206e35a3
Randy <zhangzp9970@outlook.com>
2021-08-14 15:05:10 +0800

    add two init functions

--- torchplus/utils/init.py
+++ torchplus/utils/init.py
@@ -1,10 +1,14 @@
-import random
+import datetime
 import os
+import random
+
 import numpy as np
 import torch
 import torch.backends.cudnn as cudnn
+from torch.utils.tensorboard import SummaryWriter
 
-def init_seed(seed:int)->None:
+
+def init_seed(seed: int) -> None:
     cudnn.benchmark = True
     cudnn.deterministic = True
     seed = 9970
@@ -14,8 +18,22 @@ def init_seed(seed:int)->None:
     np.random.seed(seed)
     os.environ['PYTHONHASHSEED'] = str(seed)
 
-def auto_save_file(filename,target_dir)->None:
+
+def auto_save_file(filename, target_dir) -> None:
     with open(os.path.join(target_dir, os.path.basename(filename)), 'w', encoding='utf-8') as fout:
         with open(os.path.abspath(filename), 'r', encoding='utf-8') as fin:
             file = fin.read()
             fout.write(file)
+
+
+def get_device() -> torch.device:
+    return torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
+def get_summary_writer(root_dir: str = None) -> SummaryWriter:
+    if root_dir is not None:
+        now = datetime.datetime.now().strftime('%b%d_%H-%M-%S')
+        log_dir = f'{root_dir}/{now}'
+        return SummaryWriter(log_dir)
+    else:
+        return SummaryWriter()
