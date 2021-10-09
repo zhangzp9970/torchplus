de727634bc0b4b90e55d5bf8fe12adad621b21e5
Randy <zhangzp9970@outlook.com>
2021-10-09 19:58:24 +0800

    fix bug

--- bld.bat
+++ bld.bat
@@ -1,2 +1,2 @@
-%PYTHON% C:\Users\zhang\Documents\GitHub\torchplus\setup.py install --single-version-externally-managed --record=record.txt
+%PYTHON% .\setup.py install --single-version-externally-managed --record=record.txt
 if errorlevel 1 exit 1
