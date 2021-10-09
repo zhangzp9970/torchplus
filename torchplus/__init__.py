4262e6399bad71ab5deb4722fdf54192df41c2dd
Randy <zhangzp9970@outlook.com>
2021-10-09 20:57:20 +0800

    fix version bug

--- torchplus/__init__.py
+++ torchplus/__init__.py
@@ -3,6 +3,7 @@ from torchplus import nn
 from torchplus import utils
 from torchplus import transforms
 from .version import __version__ as __version__
+from pkg_resources import parse_version
 try:
     from torch import __version__ as torch_version
 except ImportError:
@@ -13,7 +14,7 @@ except ImportError:
     raise RuntimeError('torchvision not found !')
 
 # check torch version and torchvision version
-if torch_version < '1.8.1':
+if parse_version(torch_version) < parse_version('1.8.1'):
     raise RuntimeWarning('Pytorch version should be greater than 1.8.1 !')
-if torchvision_version < '0.9.1':
+if parse_version(torchvision_version) < parse_version('0.9.1'):
     raise RuntimeWarning('torchvision version should be greater than 0.9.1 !')
