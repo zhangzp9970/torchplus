5af769e1d4a4c037d569b1743c8fceae3869a7ee
Randy <zhangzp9970@outlook.com>
2023-03-15 09:44:07 +0800

    add mmd

--- torchplus/utils/__init__.py
+++ torchplus/utils/__init__.py
@@ -1,6 +1,6 @@
 from .init import Init
 from .accuracy import BaseAccuracy, ClassificationAccuracy
-from .utils import class_split, save_excel, save_csv, read_image_to_tensor, hash_code
+from .utils import class_split, save_excel, save_csv, read_image_to_tensor, hash_code, MMD
 
 __all__ = ('Init', 'BaseAccuracy', 'ClassificationAccuracy',
-           'class_split', 'save_excel', 'save_csv', 'read_image_to_tensor', 'hash_code')
+           'class_split', 'save_excel', 'save_csv', 'read_image_to_tensor', 'hash_code', 'MMD')
